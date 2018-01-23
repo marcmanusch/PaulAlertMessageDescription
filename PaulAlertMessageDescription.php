08dd4d738195973caf1b3b809560ce034b9dd075
marcmanusch <31078285+marcmanusch@users.noreply.github.com>
2018-01-23 17:38:05 +0100

    Update PaulAlertMessageDescription.php

--- PaulAlertMessageDescription.php
+++ PaulAlertMessageDescription.php
@@ -6,7 +6,13 @@ use Shopware\Components\Plugin\Context\InstallContext;
 
 class PaulAlertMessageDescription extends \Shopware\Components\Plugin
 {
-    
-
+    /**
+     * @param ContainerBuilder $container
+     */
+    public function build(ContainerBuilder $container)
+    {
+        $container->setParameter('paul_alert_message_description.plugin_dir', $this->getPath());
+        parent::build($container);
+    }
 }
 ?>
