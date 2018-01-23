cde54508bf27a131616c5dd4e7c3e2ee927c693d
marcmanusch <31078285+marcmanusch@users.noreply.github.com>
2018-01-23 19:34:43 +0100

    Update Frontend.php

--- Subscriber/Frontend.php
+++ Subscriber/Frontend.php
@@ -1,11 +1,17 @@
 <?php
+
 namespace PaulAlertMessageDescription\Subscriber;
+
 use Enlight\Event\SubscriberInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+
+
 class Frontend implements SubscriberInterface
 {
+
     /** @var  ContainerInterface */
     private $container;
+
     /**
      * Frontend contructor.
      * @param ContainerInterface $container
@@ -14,38 +20,26 @@ class Frontend implements SubscriberInterface
     {
         $this->container = $container;
     }
+
     /**
      * @return array
      */
-    public static function getSubscribedEvents()
+    public static function getSubscribedEvents(): array
     {
         return [
             'Enlight_Controller_Action_PostDispatchSecure_Frontend' => 'onFrontendPostDispatch',
         ];
     }
+
     /**
      * @param \Enlight_Event_EventArgs $args
      */
     public function onFrontendPostDispatch(\Enlight_Event_EventArgs $args)
-    {	    
-	$controller = $args->getSubject();
-    	$view = $controller->View();
-	$view->addTemplateDir($this->pluginBasePath . '/Resources/views/');
-        
-        //get config
-        $config = $this->container->get('shopware.plugin.config_reader')->getByPluginName('PaulAlertMessageDescription', $shop);
-    	if (empty($config->show)) {
-        	return;
-    	}
+    {
+        /** @var $controller \Enlight_Controller_Action */
+        $controller = $args->getSubject();
+        $view = $controller->View();
+        $view->addTemplateDir($this->container->getParameter('paul_alert_message_description.plugin_dir') . '/Resources/views');
 
-        // get plugin settings
-        $active = $config['active'];
-        $paulMessageDescription = $config['paulMessageDescription'];
-	$paulAlertType = $config['paulAlertType'];
-	    
-        // aggign to frontend
-        $view->assign('paulActiveDescriptionMessage', $active);
-        $view->assign('paulMessageDescription', $paulMessageDescription);
-	$view->assign('paulAlertType', $paulAlertType);
     }
 }
