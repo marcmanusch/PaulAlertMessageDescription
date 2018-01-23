05a4c5022d69c73b74cd7956fa3f06af1f15d33e
marcmanusch <31078285+marcmanusch@users.noreply.github.com>
2018-01-23 17:35:16 +0100

    Update PaulAlertMessageDescription.php

--- PaulAlertMessageDescription.php
+++ PaulAlertMessageDescription.php
@@ -6,44 +6,7 @@ use Shopware\Components\Plugin\Context\InstallContext;
 
 class PaulAlertMessageDescription extends \Shopware\Components\Plugin
 {
-    public static function getSubscribedEvents() {
-        return[
-			'Theme_Compiler_Collect_Plugin_Less' => 'addLessFiles',
-            'Enlight_Controller_Action_PostDispatchSecure_Frontend_Detail' => 'onPostDispatch'
-        ];
-    }
-	
-	public function addLessFiles(\Enlight_Event_EventArgs $args)
-    {
-
-        $less = new \Shopware\Components\Theme\LessDefinition([], [__DIR__ . '/Resources/views/frontend/_public/src/less/all.less']);
-        return new \Doctrine\Common\Collections\ArrayCollection([$less]);
-
-    }
-
-
-    public function onPostDispatch(\Enlight_Event_EventArgs $args)
-    {
-        $this->container->get('Template')->addTemplateDir(
-            $this->getPath() . '/Resources/views/'
-        );
-
-        $controller = $args->get('subject');
-        $view = $controller->View();
-
-        //check if Plugin active
-        $config = $this->container->get('shopware.plugin.config_reader')->getByPluginName($this->getName());
-
-        $active = $config['active'];
-        $paulMessageDescription = $config['paulMessageDescription'];
-	$paulAlertType = $config['paulAlertType'];
-	    
-
-        $view->assign('paulActiveDescriptionMessage', $active);
-        $view->assign('paulMessageDescription', $paulMessageDescription);
-	$view->assign('paulAlertType', $paulAlertType);
-
-    }
+    
 
 }
 ?>
